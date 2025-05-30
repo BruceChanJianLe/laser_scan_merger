7b442645cf860d5939c6c77a10a63ce57bd4cd3c
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-05-30 18:16:32 +0800

    wip: add config, rviz and launch dirs

---
+++ launch/start_demo.launch.py
@@ -0,0 +1,73 @@
+from launch import LaunchDescription, LaunchContext
+from launch_ros.actions import ComposableNodeContainer, Node
+from launch_ros.descriptions import ComposableNode
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import ExecuteProcess, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from os import path
+
+ARGUMENTS = [
+    DeclareLaunchArgument(
+        'robotname',
+        default_value='example',
+        description='Pass robotname to select configuration for merger',
+    )
+]
+
+def generate_launch_description():
+
+    container = ComposableNodeContainer(
+        package="rclcpp_components",
+        executable="component_container",
+        name="component_manager_node",
+        namespace="",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="laser_scan_merger",
+                plugin="util::LaserScanMerger",
+                name="laser_scan_merger_node",
+                parameters=[PathJoinSubstitution([
+                    get_package_share_directory('laser_scan_merger'),
+                    'config',
+                    LaunchConfiguration('robotname'),
+                    'param.yaml'
+                ])]
+            )
+        ],
+        output="screen"
+    )
+
+    # Needed for bag that does not have clock
+    # start_pc_repub = Node(
+    #     package="laser_scan_merger",
+    #     executable="point_cloud_repub",
+    #     output="screen",
+    # )
+
+    start_rviz2 = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        arguments=[
+            "-d", path.join(get_package_share_directory("laser_scan_merger"),
+            "rviz", "demo.rviz")
+        ],
+    )
+
+    play_rosbag = ExecuteProcess(
+        cmd=[
+            "ros2", "bag", "play", "-l",
+            path.join(get_package_share_directory("laser_scan_merger"),
+            "test", "demo_scan"
+        )],
+        shell=True,
+        output="screen"
+    )
+
+    ld = LaunchDescription(ARGUMENTS)
+    ld.add_action(container)
+    # ld.add_action(start_pc_repub)
+    ld.add_action(start_rviz2)
+    ld.add_action(play_rosbag)
+
+    return ld
